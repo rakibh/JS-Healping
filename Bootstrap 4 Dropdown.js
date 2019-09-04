c234f346cbfa3a7fb57eaf282f834728378d1b18
Rakib Hasan <mrakib22@gmail.com>
2019-09-04 23:19:37 +0600

    Bootstrap 4 dowpdown
    This code for customize bootstrap 4 dropdown.

---
+++ Bootstrap 4 Dropdown.js
@@ -0,0 +1,31 @@
+/*----------------------------------------------------------
+                    Dropdown menu show mouseover
+-----------------------------------------------------------*/
+
+$(".dropdown-menu").css('margin-top', 0);
+$(".dropdown")
+    .mouseover(function () {
+        $(this).addClass('show').attr('aria-expanded', "true");
+        $(this).find('.dropdown-menu').addClass('show');
+    })
+    .mouseout(function () {
+        $(this).removeClass('show').attr('aria-expanded', "false");
+        $(this).find('.dropdown-menu').removeClass('show');
+    });
+
+
+/*----------------------------------------------------------
+        Dropdown menu show mouseover with slide effect
+-----------------------------------------------------------*/
+
+// Add slideDown animation to Bootstrap dropdown when expanding.
+$('.dropdown').mouseover(function () {
+
+    $(this).toggleClass('current').find('.dropdown-menu').stop().slideDown('fast');
+});
+
+// Add slideUp animation to Bootstrap dropdown when collapsing.
+$('.dropdown').mouseout(function () {
+
+    $(this).toggleClass('current').find('.dropdown-menu').stop().slideUp('fast');
+});
